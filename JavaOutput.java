a6011e2c747e933c7262598ca67dde8ded2c2d9b
PROGRAMMERvictor23 <okoliv23@gmail.com>
2026-02-18 12:15:07 +0100

    UserInput

--- JavaOutput.java
+++ JavaOutput.java
@@ -1,7 +1,17 @@
 public class JavaOutput{
 
 public static void main(String[] args){
-	System.out.println("Hello world,this is my first java program");  
+	//using the println method
+	System.out.println("Hello world,this is my first java program");
+	System.out.println("learning java is fun");
+	
+	
+	//using the print method
+	System.out.print("Do you love \"learning java\"? ");
+	System.out.print("yes I love learning java\n ");
+	
+	//using the printf method
+	System.out.printf("My name is Mercy Ben,I am %d years old %n",25);
 	}
 
 
