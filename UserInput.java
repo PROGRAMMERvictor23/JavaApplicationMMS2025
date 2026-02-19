113e9b2adf2cbb679e14c80d8f68471aaa344dbf
PROGRAMMERvictor23 <okoliv23@gmail.com>
2026-02-19 12:07:59 +0100

    OperatorsPart1

--- UserInput.java
+++ UserInput.java
@@ -9,9 +9,15 @@ public class UserInput{
 		
 		System.out.print("Enter your address: ");
 		String address = scan.nextLine();
-		
+		   
 		System.out.print("Enter your age: ");
 		int age = scan.nextInt();
+		
+		System.out.print("Enter your gender(Male/Female: ");
+		char gender = scan.next().charAt(0);
+		
+		System.out.printf("Are you happy to learn java?(true/false: ");
+		boolean ishappy = scan.nextBoolean();
 	    
 		
 		System.out.println("");
@@ -21,6 +27,7 @@ public class UserInput{
 		System.out.printf("Hello %s ,How are you today?%n",fullName);
 		System.out.printf("Nice meeting you, you are living in %s%n", address);
 		System.out.printf("%s, you are %d years old",fullName,age);
-	 
+		System.out.printf("you are a %s%n " ,gender);
+		System.out.printf("Are you happy to learn java? %b%n ",ishappy);
 	 }
 }
